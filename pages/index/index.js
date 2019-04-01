c59d8a74e9bb3beccbf2ff449ae56f1444b2f0a2
CHEN_JianShu <n551wv1@163.com>
2019-04-01 20:58:49 +0800

    新增轮播图（swiper）组件

---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+const app = getApp()
+Page({
+  data: {
+    swipers: [
+      { url: 'http://aliyun121.icjs.ink/resource/iwhale-ui/iw-swiper.png' },
+      { url: 'http://aliyun121.icjs.ink/resource/iwhale-ui/iw-swiper.png' }
+    ]
+  }
+})
