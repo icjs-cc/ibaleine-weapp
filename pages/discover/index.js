448fbce8fb6a26c2a40924963c4679666e71f8f8
CHEN_JianShu <n551wv1@163.com>
2019-05-04 13:07:46 +0800

    新增标签页组件

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -1,66 +1,24 @@
 // pages/discover/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs: [{
+      key: 1,
+      title: '最新'
+    }, {
+      key: 2,
+      title: '红茶'
+    }, {
+      key: 3,
+      title: '绿茶'
+    }, {
+      key: 4,
+      title: '岩茶'
+    }],
+    current: 1
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChangeTab({ detail } = {}) {
+    this.setData({
+      current: detail.key
+    })
   }
 })
